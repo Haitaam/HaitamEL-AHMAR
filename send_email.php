81bee94335d7c2b52d4bbcf25b9209b96e7d772a
Haitam EL AHMAR <haitamelahmar1@gmail.com>
2024-02-23 18:43:20 +0100

    v3

---
+++ send_email.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupérer les données du formulaire
+    $email = $_POST['email'];
+    $message = $_POST['textarea'];
+
+    // Adresse e-mail de destination
+    $to_email = 'haitamelahmar1@gmail.com';
+
+    // Sujet de l'e-mail
+    $subject = 'Nouveau message de votre site Web';
+
+    // Contenu de l'e-mail
+    $email_body = "Email: $email\n\nMessage:\n$message";
+
+    // En-têtes de l'e-mail
+    $headers = "From: $email";
+
+    // Envoyer l'e-mail
+    if (mail($to_email, $subject, $email_body, $headers)) {
+        echo 'Email envoyé avec succès !';
+    } else {
+        echo 'Une erreur est survenue lors de l\'envoi de l\'e-mail.';
+    }
+}
+?>

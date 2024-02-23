f79a2bb0e86cbb133fabd1b197dd3369de5d4843
Haitam EL AHMAR <haitamelahmar1@gmail.com>
2024-02-23 18:47:37 +0100

    v3

--- send_email.php
+++ send_email.php
@@ -18,7 +18,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Envoyer l'e-mail
     if (mail($to_email, $subject, $email_body, $headers)) {
-        echo 'Email envoyé avec succès !';
+        // Rediriger après l'envoi de l'e-mail
+        header("Location: /index.html"); // Remplacez thank_you.html par le chemin de votre page de remerciement
+        exit;
     } else {
         echo 'Une erreur est survenue lors de l\'envoi de l\'e-mail.';
     }
